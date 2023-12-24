56323b717ec90c53590b1800ea84fd87c0a3f820
Abdo saf <noodsaf01@gmail.com>
2023-12-24 12:27:44 +0100

    first version

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -12,7 +12,6 @@ function Projects(){
     setIsOpen(!isOpen);
   }
   
-  // console.log(window.location.pathname);  
   let path = window.location.pathname;
 
 return(
@@ -24,8 +23,8 @@ return(
           <nav>
           <ul className='flex gap-10 items-center'>
             <li><Link to="/" className='textLink  '>Home</Link></li>
-            <li><Link  to="/Projects" className='textBase {path===to ? active : "" }'>PROJECTS</Link></li>
-            <li><Link  to='/AboutME' className='textBase  '>ABOUT ME</Link></li>
+            <li><Link  to="/Projects" className='textBase {path===to ? active : "" } '>PROJECTS</Link></li>
+            <li><Link  to='/AboutME' className='textBase '>ABOUT ME</Link></li>
             <button onClick={clickMenuSvg} className='md:hidden'>
               {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" fill='#cdf381' viewBox="0 0 448 512" className='w-6 cursor-pointer menuSvg' strokeWidth={0.5}><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
                 : <svg xmlns="http://www.w3.org/2000/svg" fill='#cdf381' viewBox="0 0 384 512" className='w-6 cursor-pointer'><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>}
@@ -74,9 +73,9 @@ return(
 </svg>
 
 </div>
-<div className="textSvg lo:my-40 lg:my-32 xl:my-12  ">
-<h3 className="text-blue text-center  lo:text-4xl md:text-5xl lg:text-7xl xl:text-9xl xl:px-8 xl:py-12">Experience the Future <span className="block lo:mt-5 lo:text-2xl md:text-4xl lg:text-5xl lg:mt-7 xl:hidden">CNTIC Club's Event Series</span></h3>
-<p className="text-blue hidden xl:block text-center text-7xl">CNTIC Club's Event Series</p>
+<div className="textSvg lo:my-40 lg:my-28 xl:my-16  ">
+<h3 className="text-blue text-center  lo:text-4xl md:text-5xl lg:text-5xl xl:text-6xl xl:px-8 xl:py-12">Experience the Future <span className="block lo:mt-5 lo:text-2xl md:text-4xl lg:text-5xl lg:mt-7 xl:hidden">CNTIC Club's Event Series</span></h3>
+<p className="text-blue hidden xl:block text-center text-6xl">CNTIC Club's Event Series</p>
 </div>
 
 <div className="lastSvg flex justify-between items-end">
